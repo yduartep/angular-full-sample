a52abfa80da2c09f53fe474dbebf7b8018c10f6b
yduartep <Yahima.Duarte.Perez@everis.com>
2018-01-14 22:16:24 +0100

    setting service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,13 +13,24 @@ export class AppComponent {
 
   constructor(@Inject('defaultLanguage') private defaultLanguage: string,
               private authHelper: AuthHelper,
-              private translate: TranslateService) {
+              private translate: TranslateService,
+              private swUpdate: SwUpdate) {
 
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang(defaultLanguage);
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use(localStorage['language'] || defaultLanguage);
+
+    console.log('Initializing the app component ...');
+    if (this.swUpdate.isEnabled) {
+
+      this.swUpdate.checkForUpdate();
+      this.swUpdate.available.subscribe(event => {
+        console.log('A newer version is now available. Refresh the page now to update the cache');
+        window.location.reload();
+      });
+    }
   }
 
   isActive(): boolean {
